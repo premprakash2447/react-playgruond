88efc94caa05a55aaffd2000f46f77c0e25ce032
Prem Patel <prempatel2447@gmail.com>
2024-08-14 21:58:47 +0530

    Formik lib initial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DataList from './components/DataList'
+import FormikForm from './components/formik/FormikForm'
 
 // function App() {
   // const [count, setCount] = useState(0)
@@ -36,7 +37,8 @@ import DataList from './components/DataList'
 const App = () => {
   return (
     <div className='app'>
-      <DataList />
+      {/* <DataList /> */}
+      <FormikForm />
     </div>
   )
 }
