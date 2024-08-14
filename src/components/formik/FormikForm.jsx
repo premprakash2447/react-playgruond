2a9753fb84f97c2e155ea6fa514c1c2877f22560
Prem Patel <prempatel2447@gmail.com>
2024-08-14 22:31:40 +0530

    Form field validation without any 3rd party lib

--- src/components/formik/FormikForm.jsx
+++ src/components/formik/FormikForm.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { useFormik } from 'formik';
 
+ // A custom validation function. This must return an object
+ // which keys are symmetrical to our values/initialValues
+ const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
 const FormikForm = () => {
-// Note that we have to initialize ALL of fields with values. These
-   // could come from props, but since we don’t want to prefill this form,
-   // we just use an empty string. If we don’t do this, React will yell
-   // at us.
-   const formik = useFormik({
+  const formik = useFormik({
     initialValues: {
-      firstName: 'Prem',
-      lastName: 'Patel',
-      email: 'prem@gmail.com',
+      firstName: '',
+      lastName: '',
+      email: '',
     },
+    validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -24,8 +46,12 @@ const FormikForm = () => {
         name="firstName"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.firstName}
       />
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -33,8 +59,12 @@ const FormikForm = () => {
         name="lastName"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.lastName}
       />
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
 
       <label htmlFor="email">Email Address</label>
       <input
@@ -42,8 +72,12 @@ const FormikForm = () => {
         name="email"
         type="email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <button type="submit">Submit</button>
     </form>
